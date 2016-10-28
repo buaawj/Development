ecf8d92aa12268c442dc242608aff8cf65452bb0
buaawj <jiechiehwu@gmail.com>
2016-10-28 02:26:01 -0700

    Update Graph Parser

--- src/taskMapping/GraphParser.cpp
+++ src/taskMapping/GraphParser.cpp
@@ -17,272 +17,73 @@
 #include <string>
 #include <sstream>
 #include <cstdlib>
-#include "TaskMapping.h"
-#include "../GlobalParams.h"
-
-#include <time.h>
+#include "GraphParser.h"
 
 using namespace std;
-
+extern TaskMapping *tmInstance;
 /***************************************************************************************
  * Graph Parser Member Functions 
  ***************************************************************************************/
-
-void TaskMapping::showPEStatistics(void)
-{
-    tm_peexec_statistics<int_cycles> stats_acum;
-
-    for (map<int, tm_pe>::iterator it = tm_pes.begin(); it != tm_pes.end(); ++it)
-    {
-        it->second.printStats();
-
-        stats_acum = stats_acum + it->second.stats;
-    }
-
-    TM_SIMSTATSLOG("*** pe overall ***" << endl);
-    TM_SIMSTATSLOG("Total FLITs Transmitted=" << stats_acum.statGetFlitsTx() << endl
-                                              << "Total FLITs Received=" << stats_acum.statGetFlitsRx() << endl
-                                              << "Total Context Switching Cycles=" << stats_acum.statGetCSwitchingCycles() << endl
-                                              << "Total Task Execution Cycles=" << stats_acum.statGetTskExecCycles() << endl
-                                              << "Total PE Idle Cycles=" << stats_acum.statGetIdleCycles() << endl
-                                              << "Total Mapped Task Cycles=" << stats_acum.statGetPeTotalCycles() << endl
-                                              << "Total Energy (J)=" << stats_acum.statGetTotalEnergy_d() << endl
-                                              << "      Context Switching Energy (J)=" << stats_acum.statGetCSwitchingEnergy_d() << endl
-                                              << "      Task Execution Energy (J)=" << stats_acum.statGetTskExecEnergy_d() << endl
-                                              << "      Idle Energy (J)=" << stats_acum.statGetIdleEnergy_d() << endl);
-    TM_SIMSTATSLOG(endl);
-}
-
-/*
- * Public functions 
- */
-
-bool GraphParser::Parser(const char *fname)
+bool GraphParser::Parse(const char *fname)
 {
     ifstream infile(fname);
-    const string strapp(TM_MFILE_STRAPP);
-    const string strtask(TM_MFILE_STRTASK);
-    const string strmap(TM_MFILE_STRMAP);
-    const string strsim(TM_MFILE_STRSIM);
-    char *nextChar;
-    long int valueRead;
-    int appid = TM_NOT_APP;
+    FSM_PARSER_DATA_TABLE_STATE state = FSM_PARSER_DATA_TABLE_IDLE;
 
-    TM_SIMCONFIGLOG(endl
-                    << "************************" << endl
-                    << "Parsing TGFF Graph" << endl
-                    << "************************" << endl
-                    << endl);
+    cout << endl;
+    cout <<"************************" << endl;
+    cout <<"Parsing TGFF Graph" << endl;
+    cout <<"************************" << endl;
+    cout <<endl;
 
+    // Create appliation first
+    int appId = 1;
+    tmInstance->addApp(appId, 100000);
     if (infile.is_open())
     {
         // Read every line of the file
         while (infile)
         {
             string line;
-
+           
             if (!getline(infile, line))
                 break;
 
-            // Parser Task Table
-            if ( GetFirstString(line).compare("TASK") == 0)
+            string keyWord = GetFirstString(line);
+            cout<<"**"<<keyWord<< "  " <<keyWord.size() <<endl;
+            if (keyWord.compare("TASK") == 0)
             {
-                // Task Table
-                line = line.substr(strapp.length(), line.length() - strapp.length());
-                TM_SIMCONFIGLOG("TASK Table: " <<endl << line << endl);
-
-                istringstream segline(line);
-                tm_mfile_app_fsm app_fsm = FSMAPP_APPID;
-                int_cycles min_restart = 0;
-
-                while (segline)
-                {
-                    string seg;
-
-                    if (!getline(segline, seg, TM_MFILE_SEP))
-                        break;
-
-                    valueRead = strtol(seg.c_str(), &nextChar, 10);
-                    TM_ASSERT(*nextChar == 0, "Characters in the file are wrong!");
-
-                    switch (app_fsm)
-                    {
-                    case FSMAPP_APPID:
-                        appid = valueRead;
-                        TM_ASSERT(appid >= TM_APPID_MIN, "App identifier (" << appid << ") must be greater than or equal to " << TM_APPID_MIN << "!");
-                        app_fsm = FSMAPP_MINRESTART;
-                        break;
-                    case FSMAPP_MINRESTART:
-                        min_restart = valueRead;
-                        TM_ASSERT(min_restart >= TM_TIMERESTART_MIN, "Restart time (" << min_restart << ") must be greater or equal to " << TM_TIMERESTART_MIN << "!");
-                        addApp(appid, min_restart);
-                        app_fsm = FSMAPP_IGNORE;
-                        break;
-                    default:
-                        break;
-                    }
-                }
-            } // End if App
-            else if (line.length() >= strtask.length() && line.compare(0, strtask.length(), strtask) == 0)
+                // Task Block
+                ParseTaskBlock(appId, line);
+            }
+            else if (keyWord.compare("ARC") == 0)
             {
-                // Task
-                line = line.substr(strtask.length(), line.length() - strtask.length());
-                TM_SIMCONFIGLOG("Task: " << line << endl);
-
-                istringstream segline(line);
-                tm_mfile_task_fsm task_fsm = FSMTASK_TASKID;
-                int id = TM_NOT_TASK, staskid = TM_NOT_TASK, payload = 0;
-                int_cycles exectime = 0;
-
-                while (segline)
-                {
-                    string seg;
-
-                    if (!getline(segline, seg, TM_MFILE_SEP))
-                        break;
-
-                    valueRead = strtol(seg.c_str(), &nextChar, 10);
-                    TM_ASSERT(*nextChar == 0, "Characters in the file are wrong!");
-
-                    if (appid == TM_NOT_APP)
-                        break;
-
-                    switch (task_fsm)
-                    {
-                    case FSMTASK_TASKID:
-                        id = valueRead;
-                        TM_ASSERT(id >= TM_TASKID_MIN, "Task identifier (" << id << ") must be greater than or equal to " << TM_TASKID_MIN << "!");
-                        addTask(appid, id);
-                        task_fsm = FSMTASK_PBLOCK_ET;
-                        break;
-                    case FSMTASK_PBLOCK_ET:
-                        exectime = valueRead;
-                        TM_ASSERT(exectime >= TM_EXECTIME_MIN, "Execution Time (" << exectime << ") cannot be less than " << TM_EXECTIME_MIN << "!");
-                        task_fsm = FSMTASK_PBLOCK_ST;
-                        break;
-                    case FSMTASK_PBLOCK_ST:
-                        staskid = valueRead;
-                        TM_ASSERT(staskid >= TM_NOT_TASK, "Subsequent Task ID (" << staskid << ") shouldn't be less than " << TM_NOT_TASK << "!");
-                        task_fsm = FSMTASK_PBLOCK_PL;
-                        break;
-                    case FSMTASK_PBLOCK_PL:
-                        if (staskid != TM_NOT_TASK)
-                        {
-                            payload = valueRead;
-                            TM_ASSERT(payload >= TM_PAYLOAD_MIN, "Payload (" << payload << ") cannot be less than " << TM_PAYLOAD_MIN << "!");
-                        }
-                        else
-                            payload = 0;
-                        addPBlockToTask(appid, id, exectime, staskid, payload);
-                        task_fsm = FSMTASK_PBLOCK_ET;
-                        break;
-                    }
-                }
-            } // End if Task
-            else if (line.length() >= strmap.length() && line.compare(0, strmap.length(), strmap) == 0)
+                // ARC Block
+                ParseArcBlock(appId, line);
+            }
+            else if (keyWord.compare("@COMMUN") == 0 || state != FSM_PARSER_DATA_TABLE_IDLE)
             {
-                // MAP
-                line = line.substr(strmap.length(), line.length() - strmap.length());
-                TM_SIMCONFIGLOG("MAP: " << line << endl);
-                istringstream segline(line);
-                tm_mfile_map_fsm map_fsm = FSMMAP_START;
-                int taskid = TM_NOT_TASK, peid = TM_NOT_PE;
-                int_cycles start = 0, stop = 0;
-                int nmapping = tm_apps[appid].app_mappings.size();
-
-                while (segline)
+                // Execute time and communication volume table
+                switch (state)
                 {
-                    string seg;
-
-                    if (!getline(segline, seg, TM_MFILE_SEP))
-                        break;
-
-                    valueRead = strtol(seg.c_str(), &nextChar, 10);
-                    TM_ASSERT(*nextChar == 0, "Characters in the file are wrong!");
-
-                    switch (map_fsm)
-                    {
-                    case FSMMAP_START:
-                        start = valueRead;
-                        TM_ASSERT(start >= TM_MAPSTART_MIN, "Start Time (" << start << ") cannot be less than " << TM_MAPSTART_MIN << " in app" << appid << ", mapping" << nmapping << "!");
-                        map_fsm = FSMMAP_STOP;
-                        break;
-                    case FSMMAP_STOP:
-                        stop = valueRead;
-                        TM_ASSERT(stop > start, "Stop Time (" << stop << ") must be greater than Start Time (" << start << ") in app" << appid << " mapping" << nmapping << "!");
-                        addMap(appid, start, stop);
-                        map_fsm = FSMMAP_TASKID;
-                        break;
-                    case FSMMAP_TASKID:
-                        taskid = valueRead;
-                        TM_ASSERT(taskid >= TM_TASKID_MIN, "Task identifier (" << taskid << ") must be greater than or equal to " << TM_TASKID_MIN << " in app" << appid << ", mapping" << nmapping - 1 << "!");
-                        map_fsm = FSMMAP_PEID;
-                        break;
-                    case FSMMAP_PEID:
-                        peid = valueRead;
-                        TM_ASSERT(peid >= TM_PEID_MIN, "PE identifier (" << peid << ") must be greater than or equal to " << TM_PEID_MIN << " in app" << appid << ", mapping" << nmapping - 1 << "!");
-                        addTaskToMap(appid, taskid, peid);
-                        map_fsm = FSMMAP_TASKID;
-                        break;
-                    }
+                case FSM_PARSER_DATA_TABLE_IDLE:
+                    state = FSM_PARSER_DATA_TABLE_HEADER;
+                    break;
+                case FSM_PARSER_DATA_TABLE_HEADER:
+                    if (line.find("Type") != string::npos)
+                        state = FSM_PARSER_DATA_TABLE_DATA;
+                    break;
+                case FSM_PARSER_DATA_TABLE_DATA:
+                    ParseDataBlock(line);
+                    break;
+                default:
+                    break;
                 }
-            } // End if MAP
-            else if (line.length() >= strsim.length() && line.compare(0, strsim.length(), strsim) == 0)
-            {
-                // SIM
-                line = line.substr(strsim.length(), line.length() - strsim.length());
-                TM_SIMCONFIGLOG("SIM: " << line << endl);
-
-                istringstream segline(line);
-                tm_mfile_sim_fsm sim_fsm = FSMSIM_SCHEDULER_TICK;
-                int sch_tick = TM_PE_SCHEDULER_TICK, sch_delay = TM_PE_SCHEDULER_DELAY, sch_mapbefore = TM_PE_SCHEDULER_MAPBEFORE, sch_mappinggap = TM_PE_SCHEDULER_MAPPINGGAP;
-
-                while (segline)
-                {
-                    string seg;
-
-                    if (!getline(segline, seg, TM_MFILE_SEP))
-                        break;
-
-                    valueRead = strtol(seg.c_str(), &nextChar, 10);
-                    TM_ASSERT(*nextChar == 0, "Characters in the file are wrong!");
-
-                    switch (sim_fsm)
-                    {
-                    case FSMSIM_SCHEDULER_TICK:
-                        sch_tick = valueRead;
-                        TM_ASSERT(sch_tick >= TM_PE_SCHEDULER_TICK_MIN, "Scheduler Tick (" << sch_tick << ") must be greater than or equal to " << TM_PE_SCHEDULER_TICK_MIN << "!");
-                        sim_fsm = FSMSIM_SCHEDULER_DELAY;
-                        break;
-                    case FSMSIM_SCHEDULER_DELAY:
-                        sch_delay = valueRead;
-                        TM_ASSERT(sch_delay >= TM_PE_SCHEDULER_DELAY_MIN, "Scheduler Delay (" << sch_delay << ") must be greater than or equal to " << TM_PE_SCHEDULER_DELAY_MIN << "!");
-                        sim_fsm = FSMSIM_SCHEDULER_MAPBEFORE;
-                        break;
-                    case FSMSIM_SCHEDULER_MAPBEFORE:
-                        sch_mapbefore = valueRead;
-                        TM_ASSERT(sch_mapbefore >= TM_PE_SCHEDULER_MAPBEFORE_MIN, "Scheduler Map Before cycles (" << sch_mapbefore << ") must be greater than or equal to " << TM_PE_SCHEDULER_MAPBEFORE_MIN << "!");
-                        sim_fsm = FSMSIM_SCHEDULER_MAPPINGGAP;
-                        break;
-                    case FSMSIM_SCHEDULER_MAPPINGGAP:
-                        sch_mappinggap = valueRead;
-                        TM_ASSERT(sch_mappinggap >= TM_PE_SCHEDULER_MAPPINGGAP_MIN, "Scheduler Mapping Gap cycles (" << sch_mappinggap << ") must be greater than " << TM_PE_SCHEDULER_MAPPINGGAP_MIN << "!");
-                        scheduler_tick = sch_tick;
-                        scheduler_delay = sch_delay;
-                        scheduler_mapbefore = sch_mapbefore;
-                        scheduler_mappinggap = sch_mappinggap;
-                        sim_fsm = FSMSIM_SCHEDULER_IGNORE;
-                        break;
-                    default:
-                        break;
-                    }
-                }
-            } // End if SIM
-        }
+            }
+        } // End while
 
         TM_ASSERT(infile.eof(), "The task mapping file was not read entirely!");
 
-        // Settle Root and Leaf Tasks
+        /* // Settle Root and Leaf Tasks
         settleRootAndLeafTasks();
 
         // Settle Antecedent Tasks
@@ -297,8 +98,8 @@ bool GraphParser::Parser(const char *fname)
         {
             performMappingOnPEs();
         }
-
-        return cc;
+        */
+        return true;
     }
     else
     {
@@ -309,105 +110,115 @@ bool GraphParser::Parser(const char *fname)
     return false;
 }
 
-tm_app *TaskMapping::getApp(int appid)
+string GraphParser::GetFirstString(string s)
 {
-    tm_app *app = NULL;
+    string ret;
+    if (s.empty())
+        return ret;
 
-    TM_ASSERT(tm_apps.count(appid) > 0, "App has not been added yet!");
-    app = &tm_apps[appid];
-
-    return app;
+    int start = 0, end = 0, n = s.length();
+    while (start < n && (s[start]==' ' || s[start]=='   '))
+        ++start;
+    if (start == n)
+        return ret;
+    for (end = start; end < n && s[end]!=' '; ++end)
+        ;
+    return s.substr(start, end - start);
 }
 
-tm_task *TaskMapping::getTask(int appid, int taskid)
+/*  State Machine Functions */
+void GraphParser::ParseTaskBlock(int appId, string line)
 {
-    tm_app *app = NULL;
-    tm_task *t = NULL;
-
-    TM_ASSERT(tm_apps.count(appid) > 0, "App has not been added yet!");
-    app = &tm_apps[appid];
-    TM_ASSERT(app->task_graph.count(taskid) > 0, "Task has not been added yet!");
-    t = &app->task_graph[taskid];
-
-    return t;
-}
-
-tm_pe *TaskMapping::getPE(int peid)
-{
-    tm_pe *pe = NULL;
-
-    if (tm_pes.count(peid) > 0)
+    string buf;
+    stringstream ss(line);
+    vector<string> tokens;
+    int taskId = 0, taskType = 0;
+    cout<< "Parse Task Block"<<endl;
+    cout<<line<<endl<<endl;
+    while (ss >> buf)
     {
-        pe = &tm_pes[peid];
+        tokens.push_back(buf);
+    }
+    cout<<"Debug"<<endl;
+    for(int i=0; i<tokens.size(); ++i)
+        cout<<tokens[i]<<endl;
+    TM_ASSERT(tokens.size() == 4, "Error parsing Task block.");
+    taskId = ExtractValue(tokens[1]);
+    taskType = atoi(tokens[tokens.size() - 1].c_str());
+
+    // Add task into App
+    tmInstance->addTask(appId, taskId);
+    taskTable[taskId] = taskType;
+}
+
+void GraphParser::ParseArcBlock(int appId, string line)
+{
+    cout<< "Parse Arc Block"<<endl;
+    cout<<line<<endl<<endl;
+
+    string buf;
+    stringstream ss(line);
+    vector<string> tokens;
+    while (ss >> buf)
+    {
+        tokens.push_back(buf);
     }
 
-    return pe;
+    TM_ASSERT(tokens.size() == 8, "Error parsing Task block.");
+    int src = ExtractValue(tokens[3]);
+    int dst = ExtractValue(tokens[5]);
+    int typeId = atoi(tokens[7].c_str());
+
+    // Add edge into App graph
+    // For now, put execute time and payload as 0, since
+    // we are going to populate this later.
+    tmInstance->addPBlockToTask(appId, src, 0, dst, 0);
 }
 
-int_cycles TaskMapping::getSchedulerTick()
+void GraphParser::ParseDataBlock(string line)
 {
-    return scheduler_tick;
+    cout<< "Parse Data Block"<<endl;
+    cout<<line<<endl<<endl;
+    string buf;
+    stringstream ss(line);
+    vector<string> tokens;
+    int taskId = 0;
+
+    while (ss >> buf)
+    {
+        tokens.push_back(buf);
+    }
+
+    TM_ASSERT(tokens.size() == 3, "Error parsing Data block.");
+    int typeId = atoi(tokens[0].c_str());
+
+    // Update data table
+    execTimeTable[typeId] = atoi(tokens[1].c_str());
+    commVolumTable[typeId] = atoi(tokens[2].c_str());
 }
 
-int_cycles TaskMapping::getSchedulerDelay()
+int GraphParser::ExtractValue(string s)
 {
-    return scheduler_delay;
+    TM_ASSERT(s.empty() == false, "Error parsing block.");
+
+    int pos = s.find('-');
+    TM_ASSERT(pos != string::npos, "Error cannot find _");
+
+    return atoi(s.substr(pos).c_str());
 }
 
-int_cycles TaskMapping::getSchedulerTxDelay()
+int sc_main(int argc, char *argv[])
 {
-    return scheduler_tx_delay;
+    GraphParser parser;
+
+    srand(time(NULL));
+    tmInstance = new TaskMapping("TaskMapping");
+    cout << endl
+         << "\t\tGraph Parser Testing" << endl;
+
+    parser.Parse("graph.tgff");
+
+    tmInstance->showAllAppsAndTasksInfo();
+
+    return 0;
 }
-
-int_cycles TaskMapping::getSchedulerMapBefore()
-{
-    return scheduler_mapbefore;
-}
-
-int_cycles TaskMapping::getSchedulerMappingGAP()
-{
-    return scheduler_mappinggap;
-}
-
-void TaskMapping::showTaskMappingConfiguration(void)
-{
-    TM_SIMCONFIGLOG(endl
-                    << "**************************" << endl
-                    << "Task Mapping Configuration" << endl
-                    << "**************************" << endl
-                    << endl);
-
-    showAllAppsAndTasksInfo();
-    showAllMappedTasksOnPEsInfo();
-}
-
-void TaskMapping::showTaskMappingStatistics(void)
-{
-    TM_SIMSTATSLOG(endl
-                   << "***********************" << endl
-                   << "Task Mapping Statistics" << endl
-                   << "***********************" << endl
-                   << endl);
-
-    showAppStatistics();
-    showPEStatistics();
-}
-
-/*
-
-int sc_main(int argc, char* argv[]) {
-  TaskMapping taskMap;
-  
-  srand(time(NULL));
-  
-  cout << endl << "\t\tNoxim - the NoC Simulator" << endl;
-  cout << "\t\t(C) University of Catania" << endl << endl;
-
-  taskMap.createTaskGraphFromFile("TaskMappingExample.txt");
-  
-  taskMap.showAllAppsAndTasksInfo();
-
-  return 0;
-}
-
-*/
--- src/taskMapping/GraphParser.h
+++ src/taskMapping/GraphParser.h
@@ -3,7 +3,7 @@
  *
  * TGFF Random Graph Parser for Task Mapping Module
  * (C) 2016 by the University of California Irvine
- 
+
  * Task Mapping Module:
  * (C) 2016 by the University of Antioquia, Colombia
  *
@@ -19,76 +19,15 @@
 
 #include <vector>
 #include <map>
-#include <queue>
-#include <iomanip>
-#include <time.h>
-#include <stdint.h>
 
-#include "TaskMappingLogs.h"
+#include "TaskMapping.h"
 #include "TaskMappingStats.h"
-#include "../DataStructs.h"
 
-#define TM_NOT_APP -1
-#define TM_NOT_ROOT -1
-#define TM_NOT_TASK -1
-#define TM_NOT_PE -1
-#define TM_START_PBLOCK -1
-#define TM_START_MAPPING -1
-
-#define TM_APPID_MIN 0
-#define TM_TASKID_MIN 0
-#define TM_PEID_MIN 0
-#define TM_TIMERESTART_MIN 0
-#define TM_EXECTIME_MIN 0
-#define TM_PAYLOAD_MIN 2 // min: header and tail flits
-#define TM_MAPSTART_MIN 0
-
-#define TM_MFILE_SEP ','
-#define TM_MFILE_STRAPP "app:"
-#define TM_MFILE_STRTASK "task:"
-#define TM_MFILE_STRMAP "map:"
-#define TM_MFILE_STRSIM "sim:"
-
-// Timing and Delays (to be included in the mapping file)
-#define TM_PE_SCHEDULER_TICK 100
-#define TM_PE_SCHEDULER_DELAY 10
-#define TM_PE_SCHEDULER_TXSTART 1
-#define TM_PE_SCHEDULER_MAPBEFORE 5
-#define TM_PE_SCHEDULER_MAPPINGGAP 10
-#define TM_PE_SCHEDULER_TICK_MIN 20
-#define TM_PE_SCHEDULER_DELAY_MIN 1
-#define TM_PE_SCHEDULER_MAPBEFORE_MIN 0
-#define TM_PE_SCHEDULER_MAPPINGGAP_MIN 10
-
-typedef int32_t int_cycles;
-
-enum tm_mfile_app_fsm
+enum FSM_PARSER_DATA_TABLE_STATE
 {
-    FSMAPP_APPID,
-    FSMAPP_MINRESTART,
-    FSMAPP_IGNORE,
-};
-enum tm_mfile_task_fsm
-{
-    FSMTASK_TASKID,
-    FSMTASK_PBLOCK_ET,
-    FSMTASK_PBLOCK_ST,
-    FSMTASK_PBLOCK_PL
-};
-enum tm_mfile_map_fsm
-{
-    FSMMAP_START,
-    FSMMAP_STOP,
-    FSMMAP_TASKID,
-    FSMMAP_PEID
-};
-enum tm_mfile_sim_fsm
-{
-    FSMSIM_SCHEDULER_TICK,
-    FSMSIM_SCHEDULER_DELAY,
-    FSMSIM_SCHEDULER_MAPBEFORE,
-    FSMSIM_SCHEDULER_MAPPINGGAP,
-    FSMSIM_SCHEDULER_IGNORE
+    FSM_PARSER_DATA_TABLE_IDLE,
+    FSM_PARSER_DATA_TABLE_HEADER,
+    FSM_PARSER_DATA_TABLE_DATA
 };
 
 /*************************************************
@@ -97,31 +36,31 @@ enum tm_mfile_sim_fsm
 class GraphParser
 {
   private:
-    unordered_map<int, int> taskTable;         // KEY: task id, task type id
-    unordered_map<int, int> execTimeTable;     // KEY: type id, execute time
-    unordered_map<int, tm_app> commVolumTable; // KEY: arc id,  communication volum
+    map<int, int> taskTable;         // KEY: task id, task type id
+    map<int, int> execTimeTable;     // KEY: type id, execute time
+    map<int, int> commVolumTable; // KEY: arc id,  communication volum
+
+    FSM_PARSER_DATA_TABLE_STATE state;
+    string GetFirstString(string s);
+    void ParseTaskBlock(int appId, string line);
+    void ParseArcBlock(int appId, string line);
+    void ParseDataBlock(string line);
+    int ExtractValue(string s);
 
   public:
-    ofstream graphFile;
-
-    GraphParser(string objectName)
+    GraphParser(void)
     {
         taskTable.clear();
         execTimeTable.clear();
         commVolumTable.clear();
     }
 
-    ~TaskMapping()
+    ~GraphParser()
     {
-        if (graphFile.is_open())
-        {
-            graphFile.close();
-        }
     }
 
     /* Member functions to parser graph */
-    bool parser(const char *fname);
-
+    bool Parse(const char *fname);
 };
 
 #endif // __NOXIM_TGFF_GRPHY_PARSER__
